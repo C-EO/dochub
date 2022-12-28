d8c3dd68877289a5bc620c42b9a94bafa2cc7b4a
Dennis Sebalemba <dennis.sebalemba@gmail.com>
2022-12-28 21:24:21 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  staticImage: true,
+  defaultShowCopyCode: true
 })
 
 module.exports = withNextra({
