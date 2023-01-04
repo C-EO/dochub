dbe1db28655c2b3f63c0f40d8c338d6a2d6385b3
Dennis Sebalemba <dennis.sebalemba@gmail.com>
2023-01-04 13:27:16 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
-import { remarkMdxDisableExplicitJsx } from 'remark-mdx-disable-explicit-jsx'
-
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   staticImage: true,
   defaultShowCopyCode: true,
-  remarkPlugins: [
-    [
-      remarkMdxDisableExplicitJsx,
-      { whiteList: ['table', 'thead', 'tbody', 'tr', 'th', 'td', 'h3', 'a'] }
-    ]
-  ]
 })
 
 module.exports = withNextra({
