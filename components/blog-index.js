41b2a9a18ea05450868a73069ab4ce728dcd6a7a
Dennis Sebalemba <dennis.sebalemba@gmail.com>
2023-02-23 04:19:39 +0300

    Update blog-index.js

--- components/blog-index.js
+++ components/blog-index.js
@@ -3,25 +3,34 @@ import Link from "next/link";
 
 export default function BlogIndex({ more = "Read more" }) {
   return getPagesUnderRoute("/blog").map((page) => {
-    // Alias `<a>` to avoid it being replaced by MDX components.
-    // const A = "a";
     return (
-      <div className="">
-      <h3>
-        <Link href={page.route} style={{ color: "inherit", textDecoration: "none" }} className="block font-semibold mt-8 text-2xl">
-          {page.meta?.title || page.frontMatter?.title || page.name}
-        </Link>
+      <div className="mb-10">
+        <h3>
+          <Link
+            href={page.route}
+            style={{ color: "inherit", textDecoration: "none" }}
+            className="block font-semibold mt-8 text-2xl "
+          >
+            {page.meta?.title || page.frontMatter?.title || page.name}
+          </Link>
         </h3>
-        <p className="opacity-80" style={{ marginTop: ".5rem" }}>
+        <p className="opacity-80 mt-6 leading-7">
           {page.frontMatter?.description}{" "}
           <span className="inline-block">
-            <Link href={page.route}>{more + " →"}</Link>
+            <Link
+              href={page.route}
+              className="text-[color:hsl(var(--nextra-primary-hue),100%,50%)] underline underline-offset-2 decoration-from-font"
+            >
+              {more + " →"}
+            </Link>
           </span>
         </p>
         {page.frontMatter?.date ? (
-          <p className="opacity-50 text-sm">{page.frontMatter.date}</p>
+          <p className="opacity-50 text-sm mt-6 leading-7">
+            {page.frontMatter.date}
+          </p>
         ) : null}
       </div>
     );
   });
-};
+}
