b263cce52cc25f8580f2d7e821ee9da3260c95d9
Dennis Sebalemba <dennis.sebalemba@gmail.com>
2023-02-23 04:45:08 +0300

    Update authors.js

--- components/authors.js
+++ components/authors.js
@@ -1,4 +1,3 @@
-
 export default function Authors({ date, children, by = "by" }) {
   return (
     <div className="mt-4 mb-16 text-gray-500 text-sm">
@@ -10,7 +9,13 @@ export default function Authors({ date, children, by = "by" }) {
 export function Author({ name, link }) {
   return (
     <span className="after:content-[','] last:after:content-['']">
-      <a key={name} href={link} target="_blank" className="mx-1 text-current">
+      <a
+        key={name}
+        href={link}
+        target="_blank"
+        // style={{ textUnderlinePosition: "under" }}
+        className="mx-1 text-current underline [text-underline-position:from-font] decoration-from-font"
+      >
         {name}
       </a>
     </span>
